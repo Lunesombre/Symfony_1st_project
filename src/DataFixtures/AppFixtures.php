2adefe72d9c52dc88fab8f9d5de28a465d9e6af8
Romain Touchet <romain.touchet@gmail.com>
2023-04-19 13:43:36 +0200

    day 2 midday

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -3,7 +3,7 @@
 namespace App\DataFixtures;
 
 use App\Entity\Article;
-use DateTime;
+use App\Entity\Category;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
 use Faker\Factory;
@@ -11,9 +11,20 @@ use Faker\Factory;
 class AppFixtures extends Fixture
 {
     private const NB_ARTICLES = 50;
+    private const NB_CATEGORY = 10;
     public function load(ObjectManager $manager): void
     {
         $faker = Factory::create();
+        
+        for ($i = 0, $categories=[]; $i < self::NB_CATEGORY; $i++) {
+            $category = new Category();
+            $category
+                ->setName($faker->word())
+                ->setDescription($faker->realTextBetween(100, 200));
+            $categories[]=$category;
+
+            $manager->persist($category);
+        }
 
         for ($i = 0; $i < self::NB_ARTICLES; $i++) {
             $article = new Article();
@@ -21,7 +32,8 @@ class AppFixtures extends Fixture
                 ->setTitle($faker->realText(35))
                 ->setDateCreated($faker->dateTimeBetween('-2 years'))
                 ->setVisible($faker->boolean(80))
-                ->setContent($faker->realTextBetween(200,500));
+                ->setContent($faker->realTextBetween(200, 500))
+                ->setCategory(($categories[$faker->numberBetween(0,count($categories)-1)]));
 
             $manager->persist($article);
         }
