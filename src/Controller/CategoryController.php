198670642916c2a4cb01d609be24ec0e8db42e66
Romain Touchet <romain.touchet@gmail.com>
2023-04-21 10:36:22 +0200

    modifs du vendredi

---
+++ src/Controller/CategoryController.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Category;
+use App\Repository\CategoryRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class CategoryController extends AbstractController
+{
+    #[Route('/categories', name: 'categories_list')]
+
+    public function list(CategoryRepository $categoryRepository): Response
+    {
+        return $this->render('category/list.html.twig', [
+            'categories' => $categoryRepository->findAll(),
+        ]);
+    }
+
+    #[Route('/categories/{id}', 'category_item')]
+    public function item(Category $category): Response {
+        return $this->render(
+            'category/item.html.twig',
+            ['category' => $category],
+        );
+    }
+}
--- src/Controller/IndexController.php
+++ src/Controller/IndexController.php
@@ -2,6 +2,8 @@
 
 namespace App\Controller;
 
+use App\Repository\ArticleRepository;
+use App\Repository\CategoryRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -9,10 +11,13 @@ use Symfony\Component\Routing\Annotation\Route;
 class IndexController extends AbstractController
 {
     #[Route('/', name: 'app_index')]
-    public function index(): Response
+    public function index(ArticleRepository $articleRepository, CategoryRepository $categoryRepository): Response
     {
+        $articles = $articleRepository->findAll();
+        $categories = $categoryRepository->findAll();
         return $this->render('index/index.html.twig', [
-            'controller_name' => 'Auricular Controller',
+            'articles' => $articles,
+            'categories' => $categories,
         ]);
     }
 }
