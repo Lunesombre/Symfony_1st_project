ef07d333e81d4f7821fda6133acacfbf697899a6
Romain Touchet <romain.touchet@gmail.com>
2023-04-24 12:40:07 +0200

    working email regsitration

--- src/Controller/NewsletterController.php
+++ src/Controller/NewsletterController.php
@@ -2,17 +2,36 @@
 
 namespace App\Controller;
 
+use App\Entity\Newsletter;
+use App\Form\NewsletterType;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class NewsletterController extends AbstractController
 {
     #[Route('/newsletter/subscribe', name: 'newsletter_subscribe')]
-    public function index(): Response
+    public function subscribe(
+        Request $request,
+        EntityManagerInterface $em): Response
     {
-        return $this->render('newsletter/index.html.twig', [
-            'controller_name' => 'NewsletterController',
+        $newsletter = new Newsletter();
+        $form = $this->createForm(NewsletterType::class, $newsletter);
+
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+            // persister mon email
+            $em->persist($newsletter);
+            $em->flush();
+            
+
+        }
+
+        // renderForm est dépréciée à partir de Symfony 6.2 et sera supprimé en 7.0, qui gérera automatiquement les form avec render() de base.
+        return $this->renderForm('newsletter/subscribe.html.twig', [
+            'form' => $form,
         ]);
     }
 }
--- src/Form/NewsletterType.php
+++ src/Form/NewsletterType.php
@@ -4,6 +4,7 @@ namespace App\Form;
 
 use App\Entity\Newsletter;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
@@ -16,7 +17,7 @@ class NewsletterType extends AbstractType
             ->add('email', EmailType::class, [
                 'required' => true
             ])
-            ->add("S'incrire", SubmitType::class)
+            ->add('inscription', SubmitType::class)
         ;
     }
 
